34cd8c943e458e34e1f5f102c3e20db16744d527
Can Aydogan <canaydogan89@gmail.com>
2011-12-15 16:12:02 +0200

    added loader interface

--- src/ObjectValidator/Mapping/Loader.php
+++ src/ObjectValidator/Mapping/Loader.php
@@ -2,7 +2,11 @@
 
 namespace ObjectValidator\Mapping;
 
+use ObjectValidator\Mapping\AbstractMetadata;
+
 interface Loader
 {
+
+    public function loadClassMetadata(AbstractMetadata $metadata);
 
 }
